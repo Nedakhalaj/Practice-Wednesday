42801e8456ab6a167e0f69cbc5902ce483a3feef
Nedakhalaj <neda.khalaj7168@gmail.com>
2026-03-25 13:12:07 +0100

    Initial Commit

---
+++ practicwendsday/practicwendsdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicwendsdayApp.swift
+//  practicwendsday
+//
+//  Created by neda khalajnejad on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct practicwendsdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
