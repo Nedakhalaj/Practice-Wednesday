1ed4b5d08016cc673f8db9633fb8c5a29d43c63c
GreedyGobling <greedygobling@gmail.com>
2026-03-25 14:55:16 +0100

    Detail view

--- practicwendsday/ContentView.swift
+++ practicwendsday/ContentView.swift
@@ -7,48 +7,93 @@
 
 import SwiftUI
 
-struct RecepiBook: Identifiable{
+struct RecepiBook: Identifiable {
     let id = UUID()
-    let title : String
-    let ingredient : [String]
-    let difficulty : String
-    let instruction : String
-    let time : Int
+    let title: String
+    let ingredient: [String]
+    let difficulty: String
+    let instruction: String
+    let time: Int
 }
 
 struct ContentView: View {
     @State var myBooks = [
-        RecepiBook(title: "Pannkakor", ingredient: ["Oil","flour","sugar"],  difficulty: "easy",
-                   instruction: "mixed flour and sugar, heat oil, pour in batter, cook on griddle",
-                   time: 15),
-        RecepiBook( title: "Lasagne", ingredient: ["meat","spices"], difficulty: "medium", instruction: "cookes layer by layer",
-                    time: 60),
-        RecepiBook( title: "Tacos",
-                    ingredient: [], difficulty: "hard", instruction: "fried meat with veggies", time: 20),
-        RecepiBook( title: "Sushibowls ", ingredient: ["fish", "rice"], difficulty: "mrdium", instruction: "raw fish with rice", time: 30)
+        RecepiBook(
+            title: "Pannkakor",
+            ingredient: ["Oil", "flour", "sugar"],
+            difficulty: "easy",
+            instruction:
+                "mixed flour and sugar, heat oil, pour in batter, cook on griddle",
+            time: 15
+        ),
+        RecepiBook(
+            title: "Lasagne",
+            ingredient: ["meat", "spices"],
+            difficulty: "medium",
+            instruction: "cookes layer by layer",
+            time: 60
+        ),
+        RecepiBook(
+            title: "Tacos",
+            ingredient: [],
+            difficulty: "hard",
+            instruction: "fried meat with veggies",
+            time: 20
+        ),
+        RecepiBook(
+            title: "Sushibowls ",
+            ingredient: ["fish", "rice"],
+            difficulty: "mrdium",
+            instruction: "raw fish with rice",
+            time: 30
+        ),
     ]
-    
+
     var body: some View {
-        NavigationStack{
-            List(myBooks){ book in
-            
-                NavigationLink{
-                    Text("test")
-                }label: {
-                    VStack{
+        NavigationStack {
+            List(myBooks) { book in
+                
+                NavigationLink {
+                    DetailView(Recipe: book)
+                } label: {
+                    VStack {
                         Text(book.title)
                             .font(.headline)
                             .foregroundColor(.primary)
                             .padding()
                         Text("\(book.time)")
-                        HStack{
+                        HStack {
                             Text("\(book.difficulty.capitalized)")
                             Spacer()
                         }
                     }
                 }
             }
+            Button("Add Recepi"){
+            }
         }
+    }
+}
+struct DetailView: View {
+    let Recipe: RecepiBook
+    var body: some View {
+        VStack{
+            Text(Recipe.title)
+            HStack{
+                Text("Igreedients:")
+                if Recipe.ingredient.count > 1 {
+                    ForEach(0..<Recipe.ingredient.count) { value in
+                        Text(Recipe.ingredient[value])
+                    }
+                }
+            }
+            Text(Recipe.instruction)
+        }
+    }
+}
+struct RecepiMaker: View{
+    let Recipe: RecepiBook
+    var body: some View{
     }
 }
 
