50bf22a372fe3ea08d3af5259df3034a85cf75d1
William Albinsson <wilabson@Williams-MacBook-Air.local>
2026-03-25 15:47:36 +0100

    Added Add recipe and swipe to delete

--- practicwendsday/ContentView.swift
+++ practicwendsday/ContentView.swift
@@ -49,27 +49,61 @@ struct ContentView: View {
         ),
     ]
 
+    @State var newTitle: String = ""
+    @State var newIngredient: [String] = [""]
+    @State var ingredientInput: String = ""
+    @State var newDifficulty: String = ""
+    @State var newInstruction: String = ""
+    @State var newTime: Int = 0
+    
     var body: some View {
         NavigationStack {
-            List(myBooks) { book in
-                
-                NavigationLink {
-                    DetailView(Recipe: book)
-                } label: {
-                    VStack {
-                        Text(book.title)
-                            .font(.headline)
-                            .foregroundColor(.primary)
-                            .padding()
-                        Text("\(book.time)")
-                        HStack {
-                            Text("\(book.difficulty.capitalized)")
-                            Spacer()
+            List {
+                ForEach(myBooks) { book in
+                    NavigationLink {
+                        DetailView(Recipe: book)
+                    } label: {
+                        VStack {
+                            Text(book.title)
+                                .font(.headline)
+                                .foregroundColor(.primary)
+                                .padding()
+                            Text("\(book.time)")
+                            HStack {
+                                Text("\(book.difficulty.capitalized)")
+                                Spacer()
+                            }
                         }
                     }
+                }.onDelete { IndexSet in
+                    myBooks.remove(atOffsets: IndexSet)
                 }
             }
-            Button("Add Recepi"){
+
+            TextField("Recipe Name", text: $newTitle)
+            TextField("Ingredients", text: $ingredientInput)
+            Button("Add ingredient") {
+                newIngredient.append(ingredientInput)
+                ingredientInput = ""
+            }
+            TextField("Recipe Difficulty", text: $newDifficulty)
+            TextField("Instructions", text: $newInstruction)
+            TextField("Cooking time", value: $newTime, format: .number)
+
+            Button("Add Recepi") {
+                myBooks.append(
+                    RecepiBook(
+                        title: newTitle,
+                        ingredient: newIngredient,
+                        difficulty: newDifficulty,
+                        instruction: newInstruction,
+                        time: newTime
+                    )
+                )
+                newTitle = ""
+                newDifficulty = ""
+                newInstruction = ""
+                newTime = 0
             }
         }
     }
@@ -77,9 +111,9 @@ struct ContentView: View {
 struct DetailView: View {
     let Recipe: RecepiBook
     var body: some View {
-        VStack{
+        VStack {
             Text(Recipe.title)
-            HStack{
+            HStack {
                 Text("Igreedients:")
                 if Recipe.ingredient.count > 1 {
                     ForEach(0..<Recipe.ingredient.count) { value in
@@ -91,9 +125,13 @@ struct DetailView: View {
         }
     }
 }
-struct RecepiMaker: View{
+struct RecepiMaker: View {
+
     let Recipe: RecepiBook
-    var body: some View{
+
+    var body: some View {
+
+        Text("Test")
     }
 }
 
